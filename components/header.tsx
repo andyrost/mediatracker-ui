e4d197cd1fab61ef98252dee7d2eafb3283b177a
Andrew Rost <andrewrost0@gmail.com>
2022-02-18 21:17:12 -0500

    small header fix

--- components/header.tsx
+++ components/header.tsx
@@ -38,34 +38,24 @@ export default function header() {
     );
   } else {
     logbutton = (
-      <button className="btn btn-blue" onClick={() => signIn()}>
-        Login
-      </button>
+      <div className="flex">
+        <button className="btn btn-blue h-10 m-3" onClick={() => signIn()}>
+          Login
+        </button>
+      </div>
     );
   }
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-primary dark:bg-primary-dark p-6">
+    <nav className="fixed w-full flex items-center justify-between flex-wrap bg-primary dark:bg-primary-dark p-6">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <Image src={initalsLogo} height={60} width={90} />
         <span className="font-bold font-mono text-3xl tracking-tight">
           MediaTracker
         </span>
       </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white">
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
+      <div className="flex-grow flex items-center w-auto">
+        <div className="text-sm flex-grow">
           <Link href="/">
             <a className={router.pathname === "/" ? navlinkactive : navlink}>
               Dashboard
